9651d2f886940bbe28f38dc9d03d52657eb5a141
jkvoorhis <jhenna.voorhis@gmail.com>
2016-12-13 10:26:26 -0800

    formatting

--- plugins/plugin_repeat.py
+++ plugins/plugin_repeat.py
@@ -5,27 +5,20 @@ from collections import Counter
 
 from rtmbot.core import Plugin
 
+
 class PluginRepeat(Plugin):
-    def __init__(self,slack_client=None, plugin_config=None):
+    def __init__(self, slack_client=None, plugin_config=None):
         # because of the way plugins are called we must explicitly pass the
         # arguments to the super
-        super(PluginRepeat, self).__init__(
-            slack_client=slack_client,
-            plugin_config=plugin_config
-        )
+        super(PluginRepeat, self).__init__(slack_client, plugin_config)
         self.words = self.load_word_list("words.txt")
-
-    def load_word_list(self, filepath):
-        with open(filepath) as f:
-            words = f.read().split()
-            return words
 
     def process_message(self, data):
         print(data)
-        if (data['channel'].startswith("C") and data['type']=='message'):
+        if (data['channel'].startswith("C") and data['type'] == 'message'):
             word_counter = self.check_for_flag_words(data['text'])
             if word_counter:
-                resp = self.slack_client.api_call('im.open', user =data['user'])
+                resp = self.slack_client.api_call('im.open', user=data['user'])
                 if resp['ok']:
                     self.outputs.append([
                         resp['channel']['id'],
@@ -34,17 +27,17 @@ class PluginRepeat(Plugin):
                     ])
 
     def check_for_flag_words(self, message):
-        # type: (str) -> Dict
-        """
-
-        :rtype: Dict
-        """
         cnt = Counter()
         delims = '!"#$%&()*+,./:;<=>?@[\\]^_`{|}~\t\n\x0b\x0c\r '
         pattern = r"[{}]".format(delims)
         message_array = re.split(pattern, message.lower())
         for word in message_array:
-            word_v2 = word.replace(u"\u2019", "").replace("'", "")
-            if word_v2 in self.words:
-                cnt[word_v2] += 1
+            formatted_word = word.replace(u"\u2019", "").replace("'", "")
+            if formatted_word in self.words:
+                cnt[formatted_word] += 1
         return dict(cnt)
+
+    def load_word_list(self, filepath):
+        with open(filepath) as f:
+            words = f.read().split()
+            return words
--- run_cheese.py
+++ run_cheese.py
@@ -5,11 +5,12 @@ import yaml
 
 from rtmbot import RtmBot
 
+
 def main():
     config = yaml.load(open('rtmbot.conf', 'r'))
 
-    debug = os.getenv('CBBP_DEBUG') #bool
-    active_plugins = os.getenv('CBBP_ACTIVE_PLUGINS') #array of module paths
+    debug = os.getenv('CBBP_DEBUG')  # bool
+    active_plugins = os.getenv('CBBP_ACTIVE_PLUGINS')  # array of module paths
     slack_token = os.getenv('CBBP_SLACK_TOKEN')
 
     plugins = []
@@ -17,11 +18,11 @@ def main():
         for file in filenames:
             if file.startswith('plugin'):
                 filename = file[:-3]
-                class_name = ''.join(x.capitalize() for x in filename.split('_'))
+                class_name = ''.join(
+                    x.capitalize() for x in filename.split('_')
+                )
                 plug = [dirpath[2:], filename, class_name]
                 plugins.append('.'.join(plug))
-
-
 
     if debug is not None:
         config['DEBUG'] = debug
