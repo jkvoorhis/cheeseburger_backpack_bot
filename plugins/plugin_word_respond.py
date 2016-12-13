07d7f19870969cdddbda49d5b6cd8dd35d93c8e6
hcrowell <helen.crowell@urbanairship.com>
2016-12-13 14:20:45 -0800

    Load words.json file

--- plugins/plugin_word_respond.py
+++ plugins/plugin_word_respond.py
@@ -4,6 +4,7 @@ from rtmbot.core import Plugin
 
 from utils import word_checking as wc_utils
 
+import json
 
 class PluginWordRespond(Plugin):
     def __init__(self, slack_client=None, plugin_config=None):
@@ -13,7 +14,8 @@ class PluginWordRespond(Plugin):
             slack_client=slack_client,
             plugin_config=plugin_config
         )
-        self.words = self.load_word_list("words.txt")
+        self.words = self.load_json("words.json")
+        print(self.words)
 
     def process_message(self, data):
         # TODO: for debugging only, remove for prod
@@ -22,7 +24,7 @@ class PluginWordRespond(Plugin):
         # temporary until we allow users to opt-in to service
         if (data['channel'].startswith("C") and data['type'] == 'message'):
             word_counter = wc_utils.check_for_flag_words(
-                data['text'], self.words
+                data['text'], self.words.keys()
             )
             if word_counter:
                 resp = self.slack_client.api_call('im.open', user=data['user'])
@@ -32,8 +34,20 @@ class PluginWordRespond(Plugin):
                         'I counted you saying the following words this many '
                         'times: \n{}'.format(word_counter)
                     ])
+                    for key, subdict in self.words.iteritems():
+                        if key in word_counter:
+                            suggestion = self.words[key]
+                            self.outputs.append([
+                                resp['channel']['id'],
+                                'Try {} instead'.format(suggestion)
+                            ])
 
     def load_word_list(self, filepath):
         with open(filepath) as f:
             words = f.read().split()
             return words
+
+    def load_json(self, filepath):
+        with open(filepath) as word_file:
+            data = json.load(word_file)
+            return data
