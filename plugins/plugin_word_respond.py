6a872988f86b4676a0b09da99090153f449cb09d
jkvoorhis <jhenna.voorhis@gmail.com>
2016-12-13 14:41:19 -0800

    format slack responses better

--- plugins/plugin_word_respond.py
+++ plugins/plugin_word_respond.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import json
 
 from rtmbot.core import Plugin
 
@@ -6,13 +7,10 @@ from utils import word_checking as wc_utils
 
 
 class PluginWordRespond(Plugin):
-    def __init__(self, slack_client=None, plugin_config=None):
+    def __init__(self, **kwargs):
         # because of the way plugins are called we must explicitly pass the
         # arguments to the super
-        super(PluginWordRespond, self).__init__(
-            slack_client=slack_client,
-            plugin_config=plugin_config
-        )
+        super(PluginWordRespond, self).__init__(**kwargs)
         self.words = self.load_word_list("words.txt")
 
     def process_message(self, data):
@@ -27,13 +25,45 @@ class PluginWordRespond(Plugin):
             if word_counter:
                 resp = self.slack_client.api_call('im.open', user=data['user'])
                 if resp['ok']:
-                    self.outputs.append([
+                    mssg_kwargs = self._build_slack_message(
                         resp['channel']['id'],
-                        'I counted you saying the following words this many '
-                        'times: \n{}'.format(word_counter)
-                    ])
+                        word_counter
+                    )
+                    self.slack_client.api_call(
+                        "chat.postMessage", **mssg_kwargs
+                    )
 
     def load_word_list(self, filepath):
         with open(filepath) as f:
             words = f.read().split()
             return words
+
+    def _build_slack_message(self, channel_id, word_dict):
+        result = {
+            "channel": channel_id,
+            "as_user": "true",
+            "attachments": []
+        }
+
+        result["attachments"].append(self._build_slack_attachment(word_dict))
+
+        return result
+
+    def _build_slack_attachment(self, word_dict):
+        attachment_template = {
+            "fallback": "Breakdown of words used, and possible "
+                        "alternatives",  # fallback text
+            "pretext": "Unpacking the Words You've Used:",
+            "color": "#439FE0",
+            "fields": [],
+            "footer": "Cheeseburger Backpack",
+            "mrkdwn_in": ["fields"]
+        }
+
+        for word, count in word_dict.iteritems():
+            attachment_template["fields"].append({
+                "title": word.capitalize(),
+                "value": "Count: {count}\nAlternatives: TBD".format(count=count),
+            })
+
+        return attachment_template
